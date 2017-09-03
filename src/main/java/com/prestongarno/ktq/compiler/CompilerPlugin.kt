91870b49d94d1385743c556b48162ce3f331e4a3
preston <prestongarno@gmail.com>
2017-09-03 16:26:50 -0400

    compilation from a plugin task implemented

--- src/main/java/com/prestongarno/ktq/compiler/CompilerPlugin.kt
+++ src/main/java/com/prestongarno/ktq/compiler/CompilerPlugin.kt
@@ -21,15 +21,38 @@ class QCompilerConfig : DefaultTask() {
   }
 
   val destinationDir: File by lazy {
-    project.file("destinationDir")?:
+    project.file("destinationDir") ?:
         File("${project.buildDir.absolutePath}/generated/ktq/")
   }
 
+  var packageName: String = "com.prestongarno.ktq.generated"
+  var kotlinName: String = ""
+
+  /**
+   * TODO : Need to verify more things like write access, etc.
+   */
   override fun doLast(action: Action<in Task>?): Task {
-    if (schema != null)
-      TODO("Run the compilation on the schema:) ")
+    if (schema?.exists() != false) {
+      QCompiler.initialize()
+          .packageName(packageName)
+          .compile(schema!!)
+          .writeToFile("${destinationDir.absolutePath}/${
+          if (kotlinName.isNotEmpty())
+            kotlinName
+          else schema!!.nameWithoutExtension
+              .toJavaFileCompat()
+          }")
+    }
     return super.doLast(action)
   }
-
 }
 
+private fun String.toJavaFileCompat(): String {
+  return replace("[^a-z|A-Z|0-9]*".toRegex(), "")
+      .let {
+        val firstLetter = it.indexOfFirst { "$it".matches("[a-zA-Z]".toRegex()) }
+        if (firstLetter > 0)
+          it.substring(firstLetter)
+        else ""
+      }
+}
