a7333fc68d1389ce20220df8931098ddde6c6538
preston <prestongarno@gmail.com>
2017-09-06 22:31:55 -0400

    test assert type inherited fails

--- src/main/kotlin/com/prestongarno/ktq/compiler/QConfiguration.kt
+++ src/main/kotlin/com/prestongarno/ktq/compiler/QConfiguration.kt
@@ -16,24 +16,10 @@ interface QConfig {
  * Extension which is added by plugin to capture input
  */
 open class QCompilerConfig(val project: Project) {
-
-  @Input @JvmField var schemaProp = project.property(File::class.java)
-
-  @OutputDirectory @JvmField var targetDirProp = project.property(File::class.java)
-
-  @JvmField var packageNameProp = project.property(String::class.java)
-
-  @JvmField var kotlinNameProp = project.property(String::class.java)
-
-  fun schema(value: String) = schemaProp.set(value.asFile())
-
-  fun targetDir(value: String) {
-    targetDirProp.set(value.asFile())
-    project.logger.info("setting target directory to: '${targetDirProp.get()}'")
-  }
-
-  fun packageName(value: String) = packageNameProp.set(value)
-  fun kotlinName(value: String) = kotlinNameProp.set(value)
+  @JvmField @Input  var schema = project.property(File::class.java)
+  @JvmField @OutputDirectory var targetDir = project.property(File::class.java)
+  @JvmField var packageName = project.property(String::class.java)
+  @JvmField var kotlinName = project.property(String::class.java)
 }
 
 /**
@@ -42,8 +28,8 @@ open class QCompilerConfig(val project: Project) {
 class ConfigAdapter(configLoader: Lazy<QCompilerConfig>) : QConfig {
   private val configuration by configLoader
   @get:Input override val schema: File by lazy {
-    val prop = configuration.schemaProp
-    (if (configuration.schemaProp.isPresent
+    val prop = configuration.schema
+    (if (configuration.schema.isPresent
         && prop.get().exists()
         && prop.get().isFile
         && prop.get().canRead())
@@ -52,25 +38,20 @@ class ConfigAdapter(configLoader: Lazy<QCompilerConfig>) : QConfig {
       File.createTempFile("null", "null").apply { setReadable(false) })
   }
   @get:OutputDirectory override val targetDir: File by lazy {
-    configuration.targetDirProp.let {
-      if (it.isPresent)
-        it.get()
-      else
-        File("${QContext.project.buildDir.absolutePath}/generated/ktq/")
-    }
+    configuration.targetDir.run {
+      if (isPresent) get() else File("${QContext.project.buildDir.absolutePath}/generated/ktq/") }
   }
   override val packageName: String by lazy {
-    val value = configuration.packageNameProp
-    (if (value.isPresent && value.get().isNotEmpty())
-      value.get()
-    else
-      "com.prestongarno.ktq.schema")
+    val value = configuration.packageName
+    (if (value.isPresent && value.get().isNotEmpty()) value.get() else "com.prestongarno.ktq.schema")
   }
   override val kotlinName: String by lazy {
-    val value = configuration.kotlinNameProp
-    (if (value.isPresent && value.get().isNotEmpty())
-      value.get()
-    else configuration.schemaProp.get().nameWithoutExtension
-        .toJavaFileCompat())
+    configuration.kotlinName.run {
+      if (isPresent && get().isNotEmpty())
+        get().toString().run {
+          if (endsWith(".kt")) this else "$this.kt" }
+      else configuration.schema.get().nameWithoutExtension
+          .toJavaFileCompat()
+    }
   }
 }
