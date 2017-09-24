b05eec1f533066a284ccf112168a4abb58594754
Preston Garno <prestongarno@gmail.com>
2017-09-24 17:06:24 -0400

    added QSchemaUnion superinterface for IR QUnionTypeDef types

--- src/main/kotlin/com/prestongarno/ktq/compiler/qlang/spec/QUnionTypeDef.kt
+++ src/main/kotlin/com/prestongarno/ktq/compiler/qlang/spec/QUnionTypeDef.kt
@@ -1,13 +1,16 @@
 package com.prestongarno.ktq.compiler.qlang.spec
 
+import com.prestongarno.ktq.QSchemaUnion
 import com.squareup.kotlinpoet.TypeSpec
 
 class QUnionTypeDef(name: String, var possibleTypes: List<QDefinedType>) : QStatefulType(name, emptyList()) {
   override fun toKotlin(): TypeSpec {
     if (this.kotlinSpec == null) {
-      this.kotlinSpec = QTypeDef(name, emptyList(), possibleTypes.map {
-        QField(it.name, it, emptyList(), QDirectiveSymbol.default, true, false)
-      }).toKotlin()
+      this.kotlinSpec = TypeSpec.objectBuilder(name)
+          .addSuperinterface(QSchemaUnion::class)
+          .addProperties(possibleTypes.map {
+            QField(it.name, it, emptyList(), QDirectiveSymbol.default, true, false).toKotlin().first
+          }).build()
     }
     return this.kotlinSpec!!
   }
